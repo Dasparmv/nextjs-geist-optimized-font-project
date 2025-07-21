9469086b3ea22e20f6e3a028fad4273aab864e5f
Dasparmv <167363598+Dasparmv@users.noreply.github.com>
2025-07-21 17:20:15 -0500

    Add .next/static/chunks/src_app_supervision_page_tsx_57ae1768._.js 1753136414669

---
+++ .next/static/chunks/src_app_supervision_page_tsx_57ae1768._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ee047e55._.js",
+  "static/chunks/src_5ea0d3ec._.js"
+],
+    source: "dynamic"
+});
