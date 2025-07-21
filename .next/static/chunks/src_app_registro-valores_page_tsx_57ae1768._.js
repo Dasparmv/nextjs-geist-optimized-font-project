ca3ca46ec173aba627b1a26579bd53275b620cd1
Dasparmv <167363598+Dasparmv@users.noreply.github.com>
2025-07-21 17:20:21 -0500

    Add .next/static/chunks/src_app_registro-valores_page_tsx_57ae1768._.js 1753136421329

---
+++ .next/static/chunks/src_app_registro-valores_page_tsx_57ae1768._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_9b435b1e._.js",
+  "static/chunks/node_modules_deaf5638._.js"
+],
+    source: "dynamic"
+});
